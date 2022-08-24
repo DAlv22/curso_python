396936705714db574479b5ec09aef7fc407cbc10
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-08-23 19:33:39 -0500

    Primera subida de carpeta del curso

---
+++ curso lógica de programación/HolaMundo.py
@@ -0,0 +1,11 @@
+# EJEMPLO SALUDO
+# COMENTARIOS.- Sirven para documentar y detallar cada parte del código o programa
+# Los comentarios los omite o no los toma en cuenta el Lenguaje de Programación Phyton
+
+# IMPRIMIR O MOSTRAR EN PANTALLA LOS RESUTLADOS
+print(5+6)
+print(10-6)
+print(7*8)
+print(20/4)
+print("HolaMundo")
+print('Dulce')
