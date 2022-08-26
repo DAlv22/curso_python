7dc3385814e7fdeaaf72f14f2d27e1d8abe7f750
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-08-25 19:54:38 -0500

    Ejercicio03

---
+++ curso lógica de programación/Ejercicio03.py
@@ -0,0 +1,20 @@
+# Ejercicio 03
+# Determinar la edad de una persona conociendo el año actual y el año de nacimiento
+
+# ENTRADA DE DATOS
+fecha_de_nacimiento = input("Indique fecha_de_nacimiento: ")
+hoy = 
+''' cómo colocar la fecha de hoy'''
+
+
+# PROCESOS
+resta = fecha_de_nacimiento - hoy
+edad = resta 
+'''colocar el resultado en años'''
+
+
+
+# SALIDA DE DATOS
+print ("La edad actual de esta persona es: ", edad )
+
+
