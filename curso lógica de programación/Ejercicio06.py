f51bbd9f388e1838a3667660d766021efb37ccba
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-08-30 17:47:03 -0500

    Ejercicio06

---
+++ curso lógica de programación/Ejercicio06.py
@@ -0,0 +1,14 @@
+# Ejercicio 6
+# Pedir un número y decir si es par o impar
+
+#ENTRADA DE DATOS
+número = int(input("Coloca un número que quieras saber si es par o impar: "))
+
+
+# PROCESO # SALIDA DE DATOS
+if (número % 2 == 0):
+    print ("Este numero es par")
+else:
+    print ("Este numero es impar")
+
+
