56eb52ef7bede0aee6abf776f4e0b12dafbf66dc
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-09-03 22:57:30 -0500

    Ejercicio11

---
+++ curso lógica de programación/Ejercicio11.py
@@ -0,0 +1,15 @@
+# 11. Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+# Declarar o crear arreglos o listas
+lista_numérica = [24, 15, 64, 86, 48, 75, 32, 1, 99, 6]
+
+# PROCESOS (Operaciones o funciones aplicadas a los arreglos)
+lista_numérica.sort ()   # Ordenar valores ASC
+
+for número in lista_numérica:  
+    if (número > 50):
+        print (número)
+
+
+# Salida de datos
+print(lista_numérica) 
