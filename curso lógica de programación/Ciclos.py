60279b2f40b5de4b3985a4667b5ced38a7564e9d
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-09-03 22:59:25 -0500

    Ciclos

---
+++ curso lógica de programación/Ciclos.py
@@ -0,0 +1,30 @@
+# CICLO FOR (Para)
+for i in range(0, 5, 1):       # Función range (valor inicial, i < 5 valor final, incremente/decremento)
+    print(i, "Dulce")          # proceso
+
+
+for i in range(2, 101, 2):       
+    print(i, "Dulce")
+
+
+'''
+PRUEBA DE ESCRITORIO
+               VARIABLE    PROCESO
+                   i        imprimir
+valor inicial      0
+                   1        Dulce
+                   2        Dulce
+                   3        Dulce
+                   4        Dulce
+                   5
+valor final
+'''
+
+
+
+# CICLO WHILE (Mientras)
+j = 9                  # valor inicial
+while(j > 0):          # condición final ( j >= 1)
+  print(j, "Eduardo")  # proceso
+  j = j - 2            # OPERADOR DE incremento / decremento (j += 1)
+
