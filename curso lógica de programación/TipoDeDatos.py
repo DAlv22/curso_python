8ff8b45e0821bbfc09ffd48360953da7190b9e9d
DAlv22 <111930726+DAlv22@users.noreply.github.com>
2022-09-03 22:58:37 -0500

    TipoDeDatos

---
+++ curso lógica de programación/TipoDeDatos.py
@@ -0,0 +1,33 @@
+# EJEMPLO 3. TIPO DE DATOS
+# Se utilizan para clasificar los datos 
+
+# int, float (Numericos), string (Texto), bool (logico booleano), list (lista), tuple
+
+nombre = "Dulce"
+edad = 20
+estatura = 1.63
+teléfono = "552634534"
+dirección = "Calle X, Col Y, C.P. Z"
+correo = "dulce@gmail.com"
+colores_favoritos = ['verde', 'café', 'gris']
+# Índice (Index)        0        1        2
+words = ("cat", "burble", "sun")
+# Índice (Index)
+estatus = True # False
+
+
+print (nombre, type(nombre)) # La función type () nos muestra el tipo de datos de la variable
+print (edad, type(edad))
+print (estatura, type(estatura))
+print (teléfono, type(teléfono))
+print (correo, type(correo))
+print (colores_favoritos, type(colores_favoritos))
+print (words, type(words))
+print (estatus, type(estatus))
+
+
+# Lista: puedes modificar valores
+colores_favoritos
+# Tupla: no puedes modificar valores
+words
+
